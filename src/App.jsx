5f89a659e3e77cb3a580ffb44eed7ed3ee6797bb
feward295-cloud <feward295@gmail.com>
2026-07-19 02:50:06 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,5 @@
-import { useState } from "react";
-import AuthScreen from "./AuthScreen.jsx";
 import FewardApp from "./FewardApp.jsx";
 
 export default function App() {
-  const [user, setUser] = useState(null);
-
-  if (!user) {
-    return <AuthScreen onAuthenticated={(u) => setUser(u)} />;
-  }
-
-  return <FewardApp user={user} onLogout={() => setUser(null)} />;
+  return <FewardApp />;
 }
